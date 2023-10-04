cb68856506443493724e3e2f1ad7cfdaa46f1aa7
jGledsonSilva <gledsonsilvaoliveira10@gmail.com>
2023-10-03 23:34:35 -0300

    atl_fuck

--- trabalho_1/teste.c
+++ trabalho_1/teste.c
@@ -6,65 +6,105 @@
 #include <string.h>
 
 #define TRUE 1
-#define MAX_COMMAND_LENGTH 100
-#define MAX_PARAMETERS 10
+#define MAX_COMANDO 100
+#define MAX_PARAMETROS 10
+#define MAX_HISTORICO 1000
 
-void type_prompt()
-{
-    printf(">>> ");
-}
+char historico[MAX_HISTORICO][MAX_COMANDO];
+int posicao_historico = 0;
 
-void print_command_and_parameters(char *command, char **parameters)
+void adicionar_historico(char *comando)
 {
-    printf("Command: %s\n", command);
-    printf("Parameters:\n");
-    for (int i = 0; parameters[i] != NULL; i++)
+    if (strlen(comando) > 0)
     {
-        printf("  - %s\n", parameters[i]);
+        strcpy(historico[posicao_historico], comando);
+        posicao_historico = (posicao_historico + 1) % MAX_HISTORICO;
     }
 }
 
-void read_command(char *command, char *parameters[])
+void exibir_historico()
 {
-    char input[MAX_COMMAND_LENGTH];
+    int i, j;
+    j = posicao_historico;
+    for (i = 0; i < MAX_HISTORICO; i++)
+    {
+        if (strlen(historico[j]) > 0)
+        {
+            printf("%s\n", historico[j]);
+        }
+        j = (j + 1) % MAX_HISTORICO;
+    }
+}
+
+void ler_comando(char *comando, char *parametro[])
+{
+    char input[MAX_COMANDO];
     fgets(input, sizeof(input), stdin);
 
     input[strcspn(input, "\n")] = '\0';
 
+    if (strlen(input) > 0)
+    {
+        adicionar_historico(input);
+    }
+
     char *token = strtok(input, " ");
-    strncpy(command, token, MAX_COMMAND_LENGTH);
-
-    if (strcmp(command, "exit") == 0)
+    if (token != NULL)
     {
-        printf("\nSaindo do shell...\n\n");
-        exit(0);
-    }
+        strcpy(comando, token);
 
-    int i = 0;
-    while (token != NULL && i < MAX_PARAMETERS)
-    {
-        parameters[i] = token;
-        token = strtok(NULL, " ");
-        i++;
+        if (strcmp(comando, "exit") == 0)
+        {
+            printf("\nSaindo do shell...\n\n");
+            exit(0);
+        }
+
+        int i = 0;
+        while (token != NULL && i < MAX_PARAMETROS)
+        {
+            parametro[i] = strdup(token);
+            token = strtok(NULL, " ");
+            i++;
+        }
+        parametro[i] = NULL;
     }
-    parameters[i] = NULL;
 }
 
 int main()
 {
-    char command[MAX_COMMAND_LENGTH];
-    char *parameters[MAX_PARAMETERS];
+    char comando[MAX_COMANDO];
+    char *parametro[MAX_PARAMETROS + 1];
+    char s[100];
     int status;
 
     system("clear");
     printf("Bem vindo ao shell simplificado!\n\n");
 
-
     while (TRUE)
     {
-        type_prompt();
-        read_command(command, parameters);
-        print_command_and_parameters(command, parameters);
+        printf("%s - >", getcwd(s, 100));
+        ler_comando(comando, parametro);
+
+        if (strcmp(comando, "cd") == 0)
+        {
+            if (chdir(parametro[1]) != 0)
+            {
+                perror("Falha ao mudar o diretório");
+            }
+            else
+            {
+                char *cwd = getcwd(NULL, 0);
+                setenv("PWD", cwd, 1);
+                free(cwd);
+            }
+            continue;
+        }
+
+        if (strcmp(comando, "history") == 0)
+        {
+            exibir_historico();
+            continue;
+        }
 
         if (fork() != 0)
         {
@@ -72,9 +112,9 @@ int main()
         }
         else
         {
-            if(execvp(command, parameters) < 0)
+            if (execvp(comando, parametro) < 0)
             {
-                printf("Falha ao executar o comando.\n");
+                perror("execvp");
                 break;
             }
         }
